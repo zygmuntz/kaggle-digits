8f4f776f0e1e6d51c31f6eeb9564f43a8e190aec
zygmuntz <zajac.zygmunt@gmail.com>
2013-11-06 18:08:27 +0100

    how to run info and bugfix in digits_dataset

--- maxout/README.md
+++ maxout/README.md
@@ -3,6 +3,27 @@ Maxing out the digits
 
 See [http://fastml.com/maxing-out-the-digits/](http://fastml.com/maxing-out-the-digits/) for description.
 	
+	csv_dataset - a general dataset wrapper, not needed to run the digits example
 	digits.yaml - a config file, set your data paths here
-	digits_dataset.py - a wrapper class for the digits in CSV format
+	digits_dataset.py - a wrapper class for the digits in CSV format. You need it in your PYTHONPATH.
 	predict.py - get predictions from a model
+	
+Edit `digits.yaml` to set correct paths to training and test files.	
+	
+How to run on Windows:
+	
+	cd <project dir>
+	set PYTHONPATH=%PYTHONPATH%;.
+	set THEANO_FLAGS=device=gpu,floatX=float32
+	train.py digits.yaml
+	predict.py digits_best.pkl test.csv predictions.csv
+
+How to run on Unix:
+
+	cd <project dir>
+	export PYTHONPATH=$PYTHONPATH:.
+	export THEANO_FLAGS=device=gpu,floatX=float32
+	python train.py digits.yaml
+	python predict.py digits_best.pkl test.csv predictions.csv
+	
+	
--- maxout/csv_dataset.py
+++ maxout/csv_dataset.py
@@ -1,5 +1,6 @@
 """
-general csv dataset wrapper for pylearn2, here with one hot and scaling/255 for digits
+general csv dataset wrapper for pylearn2 
+can do automatic one-hot encoding based on labels present in a file
 """
 
 import csv
@@ -10,30 +11,27 @@ from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 from pylearn2.utils import serial
 from pylearn2.utils.string_utils import preprocess
 
-class DigitsDataset( DenseDesignMatrix ):
+class CSVDataset( DenseDesignMatrix ):
 
 	def __init__(self, 
 			path = 'train.csv',
 			one_hot = False,
-			labels = True,
-			headers = True):
+			expect_labels = True,
+			expect_headers = True):
 
 		self.path = path
 		self.one_hot = one_hot
-		# re-map those
-		self.expect_labels = labels
-		self.expect_headers = headers
+		self.expect_labels = expect_labels
+		self.expect_headers = expect_headers
 		
-		self.no_classes = 10
 		self.view_converter = None
 
 		# and go
 
 		self.path = preprocess( self.path )
 		X, y = self._load_data()
-		X = X / 255.
 		
-		super( DigitsDataset, self ).__init__( X=X, y=y )
+		super( CSVDataset, self ).__init__( X=X, y=y )
 
 	def _load_data( self ):
 	
@@ -47,11 +45,17 @@ class DigitsDataset( DenseDesignMatrix ):
 		if self.expect_labels:
 			y = data[:,0]
 			X = data[:,1:]
+			
+			# get unique labels and map them to one-hot positions
+			labels = np.unique(y)
+			labels = { x: i for i, x in enumerate( labels ) }
 
 			if self.one_hot:
-				one_hot = np.zeros(( y.shape[0], self.no_classes ), dtype='float32' )
+				one_hot = np.zeros(( y.shape[0], len( labels )), dtype='float32' )
 				for i in xrange( y.shape[0] ):
-					one_hot[i,y[i]] = 1.
+					label = y[i]
+					label_position = labels[label]
+					one_hot[i,label_position] = 1.
 				y = one_hot
 
 		else:
